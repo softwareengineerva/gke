508c8f81715bd0bfcc62d0a0a2d54504320cc51e
Jian Ouyang <jian.ouyang@sapns2.com>
2026-03-21 16:39:58 -0400

    deletion protection

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,7 +1,8 @@
 resource "google_container_cluster" "main" {
-  provider = google-beta
-  name     = var.cluster_name
-  location = var.region
+  provider            = google-beta
+  name                = var.cluster_name
+  location            = var.region
+  deletion_protection = false
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
